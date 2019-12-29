9a81f5e448038705dbfe0f4d041aaa63e6006e4e
Pulkit <aggarwalpulkit596@gmail.com>
2019-12-29 11:57:07 +0530

    add weights and setOnClickListeners

--- Lecture2/app/src/main/java/com/codingblocks/myapplication/MainActivity.kt
+++ Lecture2/app/src/main/java/com/codingblocks/myapplication/MainActivity.kt
@@ -3,16 +3,29 @@ package com.codingblocks.myapplication
 import android.content.DialogInterface
 import android.os.Bundle
 import android.util.Log
+import android.view.View
+import android.widget.Button
+import android.widget.Toast
 import androidx.appcompat.app.AlertDialog
 import androidx.appcompat.app.AppCompatActivity
 import kotlinx.android.synthetic.main.activity_main.*
 
 class MainActivity : AppCompatActivity() {
 
+//    val btn1 = findViewById<Button>(R.id.btn1)
+
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
+        //Linking of Activity class and XML(Layout)
         setContentView(R.layout.activity_main)
         Log.i("Lifecycle", "On Create Called")
+        btn1.setOnClickListener(object : View.OnClickListener{
+            override fun onClick(v: View?) {
+                Toast.makeText(this@MainActivity,"Hello",Toast.LENGTH_SHORT).show()
+            }
+
+        })
+
 ////        a("p",5,age2 = 5,msg2 =  "Pulkit")
 ////        a(msg2 =  5,age2 = "p",5,"pulkit")
 //        clickMe.setOnClickListener {
