fcdf660d5566a8cba9002fcdae57e9fd8959c49a
Pulkit <aggarwalpulkit596@gmail.com>
2020-01-05 10:41:28 +0530

    add layout by code

---
+++ Lecture4/Layouts/app/src/main/java/com/codingblocks/layouts/MainActivity.kt
@@ -0,0 +1,43 @@
+package com.codingblocks.layouts
+
+import android.os.Build
+import android.os.Bundle
+import android.widget.Button
+import android.widget.LinearLayout
+import androidx.appcompat.app.AppCompatActivity
+
+class MainActivity : AppCompatActivity() {
+
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+//        setContentView(R.layout.activity_main)
+        val ll = LinearLayout(this)
+        setContentView(ll)
+        ll.orientation = LinearLayout.VERTICAL
+        if (Build.VERSION.SDK_INT < Build.VERSION_CODES.M)
+            ll.setBackgroundColor(resources.getColor(R.color.colorAccent))
+        else
+            ll.setBackgroundColor(getColor(R.color.colorAccent))
+
+        for(i in 0..7){
+            val innerLL = LinearLayout(this)
+            innerLL.orientation = LinearLayout.HORIZONTAL
+            ll.addView(innerLL)
+            for(j in 0..7){
+                val button = Button(this)
+                button.text = "Button ${j+1}"
+                button.setBackgroundColor(getColor(R.color.colorPrimary))
+                button.background = null
+                val params = LinearLayout.LayoutParams(
+                    LinearLayout.LayoutParams.MATCH_PARENT,
+                    LinearLayout.LayoutParams.WRAP_CONTENT,
+                    1f
+                )
+                params.setMargins()
+                button.layoutParams = params
+                innerLL.addView(button)
+            }
+        }
+
+    }
+}
