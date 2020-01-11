4d5bf044ea6958dbb7e59d6f06591ca1636db04c
Pulkit <aggarwalpulkit596@gmail.com>
2020-01-11 10:27:31 +0530

    2d array of button

--- Lecture4/Layouts/app/src/main/java/com/codingblocks/layouts/MainActivity.kt
+++ Lecture4/Layouts/app/src/main/java/com/codingblocks/layouts/MainActivity.kt
@@ -5,28 +5,38 @@ import android.os.Bundle
 import android.widget.Button
 import android.widget.LinearLayout
 import androidx.appcompat.app.AppCompatActivity
-import kotlinx.android.synthetic.main.activity_main.*
 
 class MainActivity : AppCompatActivity() {
 
+
     override fun onCreate(savedInstanceState: Bundle?) {
+        val buttonArray =
+            Array(8) { i ->
+                Array(8) { j ->
+                    Button(this)
+                }
+            }
+
         super.onCreate(savedInstanceState)
-        setContentView(R.layout.activity_main)
         val ll = LinearLayout(this)
-        root.addView(ll)
+        setContentView(ll)
         ll.orientation = LinearLayout.VERTICAL
         if (Build.VERSION.SDK_INT < Build.VERSION_CODES.M)
             ll.setBackgroundColor(resources.getColor(R.color.colorAccent))
         else
             ll.setBackgroundColor(getColor(R.color.colorAccent))
 
-        for(i in 0..7){
+        for (i in 0..7) {
             val innerLL = LinearLayout(this)
             innerLL.orientation = LinearLayout.HORIZONTAL
             ll.addView(innerLL)
-            for(j in 0..7){
+            for (j in 0..7) {
                 val button = Button(this)
-                button.text = "Button ${j+1}"
+                buttonArray[i][j] = button
+                button.text = "Button ${j + 1}"
+                button.setOnClickListener {
+                    val sum = i + j
+                }
                 button.setBackgroundColor(getColor(R.color.colorPrimary))
                 button.background = null
                 val params = LinearLayout.LayoutParams(
@@ -34,11 +44,11 @@ class MainActivity : AppCompatActivity() {
                     LinearLayout.LayoutParams.WRAP_CONTENT,
                     1f
                 )
-                params.setMargins()
                 button.layoutParams = params
                 innerLL.addView(button)
             }
         }
+
 
     }
 }
