f996622a5e915cfb05cd2de05a4a2a5690c61d7e
Pulkit <aggarwalpulkit596@gmail.com>
2020-01-14 15:24:43 +0530

    add dialog with dialog extension

--- Lecture7/app/src/main/java/com/puldroid/sharedprefs/MainActivity.kt
+++ Lecture7/app/src/main/java/com/puldroid/sharedprefs/MainActivity.kt
@@ -1,9 +1,11 @@
 package com.puldroid.sharedprefs
 
 import android.content.Context
+import android.content.DialogInterface
 import android.os.Bundle
 import android.view.View
 import androidx.appcompat.app.AppCompatActivity
+import com.google.android.material.dialog.MaterialAlertDialogBuilder
 import com.google.android.material.snackbar.Snackbar
 import kotlinx.android.synthetic.main.activity_main.*
 
@@ -24,18 +26,46 @@ class MainActivity : AppCompatActivity() {
         textView.text = counter.toString()
         editText.setText(name)
         button.setOnClickListener {
+
             sharedPreferences.edit().putString("NAME", editText.text.toString()).apply()
             textView.text = counter.toString()
             sharedPreferences.edit().putInt("COUNTER", ++counter).apply()
-            Snackbar.make(root, "This is a Snackbar", Snackbar.LENGTH_SHORT)
-                .setAction("Retry") {
-                }.setActionTextColor(getColor(R.color.colorAccent))
-                .show()
+//            Snackbar.make(root, "This is a Snackbar", Snackbar.LENGTH_SHORT)
+//                .setAction("Retry ") {
+//                }.setActionTextColor(getColor(R.color.colorAccent))
+//                .show()
 
             //Extension function
-            root.showSnackbar("This is a Kotlin Snackbar")
+            this@MainActivity.showDialog("Title", "Message"){
+                root.showSnackbar("This is snackbar")
+            }
         }
+
     }
+}
+
+fun Context.showDialog(
+    title: String,
+    message: String,
+    positiveBtn: String = "Ok",
+    cancelBtn: String = "",
+    cancelable: Boolean = false,
+    function: (btn: Boolean) -> Unit = {}
+) {
+    val dialog = MaterialAlertDialogBuilder(this)
+        .setTitle(title)
+        .setMessage(message)
+        .setPositiveButton("Ok") { dialogInterface: DialogInterface, i: Int ->
+            function(true)
+        }
+    if (cancelBtn.isNotEmpty())
+        dialog.setNegativeButton(cancelBtn) { dialogInterface: DialogInterface, i: Int ->
+            function(false)
+            dialogInterface.dismiss()
+        }
+
+    dialog.setCancelable(cancelable)
+        .show()
 }
 
 fun View.showSnackbar(
