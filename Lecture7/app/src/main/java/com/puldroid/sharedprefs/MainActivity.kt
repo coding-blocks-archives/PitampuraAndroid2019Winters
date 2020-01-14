9991b843d1ec12c23af8963ed37952fcb69ac096
Pulkit <aggarwalpulkit596@gmail.com>
2020-01-14 14:49:45 +0530

    add material snackbar

--- Lecture7/app/src/main/java/com/puldroid/sharedprefs/MainActivity.kt
+++ Lecture7/app/src/main/java/com/puldroid/sharedprefs/MainActivity.kt
@@ -2,7 +2,9 @@ package com.puldroid.sharedprefs
 
 import android.content.Context
 import android.os.Bundle
+import android.view.View
 import androidx.appcompat.app.AppCompatActivity
+import com.google.android.material.snackbar.Snackbar
 import kotlinx.android.synthetic.main.activity_main.*
 
 const val prefs = "AppPrefs"
@@ -25,6 +27,31 @@ class MainActivity : AppCompatActivity() {
             sharedPreferences.edit().putString("NAME", editText.text.toString()).apply()
             textView.text = counter.toString()
             sharedPreferences.edit().putInt("COUNTER", ++counter).apply()
+            Snackbar.make(root, "This is a Snackbar", Snackbar.LENGTH_SHORT)
+                .setAction("Retry") {
+                }.setActionTextColor(getColor(R.color.colorAccent))
+                .show()
+
+            //Extension function
+            root.showSnackbar("This is a Kotlin Snackbar")
         }
     }
+}
+
+fun View.showSnackbar(
+    text: String,
+    length: Int = Snackbar.LENGTH_SHORT,
+    actionText: String = "",
+    function: () -> Unit = {}
+) {
+    val snackbar = Snackbar.make(this, text, length)
+    if (actionText.isNotEmpty()) {
+        snackbar.setAction("Retry") {
+            function()
+        }
+    } else {
+        snackbar.show()
+    }
+
+
 }
