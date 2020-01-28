aa542cbeca9c361adff7c3d2c331bb267aa7d4d3
Pulkit <aggarwalpulkit596@gmail.com>
2020-01-28 13:22:15 +0530

    add static fragment

---
+++ Lecture10/app/src/main/java/com/puldroid/fragments/BlankFragment.kt
@@ -0,0 +1,30 @@
+package com.puldroid.fragments
+
+import android.os.Bundle
+import androidx.fragment.app.Fragment
+import android.view.LayoutInflater
+import android.view.View
+import android.view.ViewGroup
+import android.widget.ArrayAdapter
+import android.widget.Toast
+import kotlinx.android.synthetic.main.fragment_blank.*
+
+
+class BlankFragment : Fragment() {
+
+    override fun onCreateView(
+        inflater: LayoutInflater, container: ViewGroup?,
+        savedInstanceState: Bundle?
+    ): View? {
+        // Inflate the layout for this fragment
+        return inflater.inflate(R.layout.fragment_blank, container, false)
+    }
+
+    override fun onViewCreated(view: View, savedInstanceState: Bundle?) {
+        super.onViewCreated(view, savedInstanceState)
+        lv.adapter = ArrayAdapter<String>(requireContext(),android.R.layout.simple_expandable_list_item_1,
+            listOf("Iron Man","Captain Marvel","THor","DOctor Strange","Hulk","Spiderman"))
+    }
+
+
+}
