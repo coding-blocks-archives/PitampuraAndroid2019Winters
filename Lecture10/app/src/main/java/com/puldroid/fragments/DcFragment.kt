bc92db15c1b70966473c1ccb03d280a55d4a07f8
Pulkit <aggarwalpulkit596@gmail.com>
2020-01-28 14:32:32 +0530

    fragments with bundle

--- Lecture10/app/src/main/java/com/puldroid/fragments/DcFragment.kt
+++ Lecture10/app/src/main/java/com/puldroid/fragments/DcFragment.kt
@@ -1,15 +1,18 @@
 package com.puldroid.fragments
 
 import android.os.Bundle
+import androidx.fragment.app.Fragment
 import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
 import android.widget.ArrayAdapter
-import androidx.fragment.app.Fragment
+import android.widget.TextView
 import kotlinx.android.synthetic.main.fragment_blank.*
 
-
-class BlankFragment : Fragment() {
+/**
+ * A simple [Fragment] subclass.
+ */
+class DcFragment : Fragment() {
 
     override fun onCreateView(
         inflater: LayoutInflater, container: ViewGroup?,
@@ -19,19 +22,10 @@ class BlankFragment : Fragment() {
         return inflater.inflate(R.layout.fragment_blank, container, false)
     }
 
-
     override fun onViewCreated(view: View, savedInstanceState: Bundle?) {
         super.onViewCreated(view, savedInstanceState)
-        if (arguments?.getString("name") == "DC")
-            lv.adapter = ArrayAdapter<String>(
-                requireContext(), android.R.layout.simple_expandable_list_item_1,
-                listOf("Superman", "Batman", "Flash", "Wonder Woman", "Aquaman")
-            )
-        else
-            lv.adapter = ArrayAdapter<String>(
-                requireContext(), android.R.layout.simple_expandable_list_item_1,
-                listOf("Iron Man", "Captain Marvel", "THor", "DOctor Strange", "Hulk", "Spiderman")
-            )
+        lv.adapter = ArrayAdapter<String>(requireContext(),android.R.layout.simple_expandable_list_item_1,
+            listOf("Superman","Batman","Flash","Wonder Woman","Aquaman"))
     }
 
 
--- Lecture10/app/src/main/java/com/puldroid/fragments/MainActivity.kt
+++ Lecture10/app/src/main/java/com/puldroid/fragments/MainActivity.kt
@@ -2,11 +2,37 @@ package com.puldroid.fragments
 
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import kotlinx.android.synthetic.main.activity_main.*
 
 class MainActivity : AppCompatActivity() {
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
+//        supportFragmentManager
+//            .beginTransaction()
+//            .add(R.id.container,BlankFragment())
+//            .commit()
+        val bundle = Bundle()
+        button.setOnClickListener {
+            val fragment = BlankFragment()
+            bundle.putString("name" , "DC")
+            fragment.arguments = bundle
+            supportFragmentManager
+                .beginTransaction()
+                .replace(R.id.container,fragment)
+                .commit()
+        }
+        button2.setOnClickListener {
+            val fragment = BlankFragment()
+            bundle.putString("name" , "Marvel")
+            fragment.arguments = bundle
+            supportFragmentManager
+                .beginTransaction()
+                .replace(R.id.container,fragment)
+                .commit()
+        }
+
+
     }
 }
