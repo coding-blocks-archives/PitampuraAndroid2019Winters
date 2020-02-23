7fa5b0954390addbb6d7266429fd8bf3b0bbc2e4
Arnav Gupta <arnav@codingblocks.com>
2020-02-23 10:12:38 +0530

    proximity sensor values
    Signed-off-by: Arnav Gupta <arnav@codingblocks.com>

--- Lecture17/HardwareSensors/app/src/main/java/com/codingblocks/hardwaresensors/MainActivity.kt
+++ Lecture17/HardwareSensors/app/src/main/java/com/codingblocks/hardwaresensors/MainActivity.kt
@@ -2,6 +2,8 @@ package com.codingblocks.hardwaresensors
 
 import android.content.Context
 import android.hardware.Sensor
+import android.hardware.SensorEvent
+import android.hardware.SensorEventListener
 import android.hardware.SensorManager
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
@@ -9,18 +11,53 @@ import android.util.Log
 
 class MainActivity : AppCompatActivity() {
 
+    lateinit var sensorEventListener: SensorEventListener
+    lateinit var sm: SensorManager
+    lateinit var proximitySensor: Sensor
+
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
 
-        val sm = getSystemService(Context.SENSOR_SERVICE) as SensorManager
+        sm = getSystemService(Context.SENSOR_SERVICE) as SensorManager
         val sensorList = sm.getSensorList(Sensor.TYPE_ALL)
 
         for (sensor in sensorList) {
-            Log.d("SENSOR", """
+            Log.d(
+                "SENSOR", """
                 ${sensor.name} 
                 ${sensor.vendor}
-            """.trimIndent())
+            """.trimIndent()
+            )
         }
+        proximitySensor = sm.getDefaultSensor(Sensor.TYPE_PROXIMITY)
+        sensorEventListener = object : SensorEventListener {
+            override fun onAccuracyChanged(sensor: Sensor?, accuracy: Int) {
+                Log.d("SENSOR", "onAccuracyChanged")
+            }
+
+            override fun onSensorChanged(event: SensorEvent?) {
+                Log.d(
+                    "SENSOR", """onSensorChanged
+                    | ${event?.values?.get(0)}
+                """.trimMargin()
+                )
+            }
+        }
+
+    }
+
+    override fun onResume() {
+        super.onResume()
+        sm.registerListener(
+            sensorEventListener,
+            proximitySensor,
+            1000 * 1000
+        )
+    }
+
+    override fun onPause() {
+        sm.unregisterListener(sensorEventListener)
+        super.onPause()
     }
 }
