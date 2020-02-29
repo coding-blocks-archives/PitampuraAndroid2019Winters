5c97fb4889397961d2ee4a6fd7d691155a516c29
pulkit-mac <aggarwalpulkit596@Gmail.com>
2020-02-29 11:54:50 +0530

    add group chat

--- Lecture18/app/src/main/java/com/puldroid/chatapp/ChatActivity.kt
+++ Lecture18/app/src/main/java/com/puldroid/chatapp/ChatActivity.kt
@@ -2,8 +2,13 @@ package com.puldroid.chatapp
 
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import android.view.View
+import com.firebase.ui.database.FirebaseListAdapter
+import com.firebase.ui.database.FirebaseListOptions
 import com.google.firebase.auth.FirebaseAuth
 import com.google.firebase.database.FirebaseDatabase
+import kotlinx.android.synthetic.main.activity_chat.*
+import kotlinx.android.synthetic.main.item_chat.view.*
 
 class ChatActivity : AppCompatActivity() {
     val db by lazy {
@@ -17,16 +22,45 @@ class ChatActivity : AppCompatActivity() {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_chat)
         val uid = auth.currentUser?.uid!!
-        db.child("messages")
+        button2.setOnClickListener {
+            db.child("messages")
 //            .child(uid)
-            .push()
-            .setValue(
-                Messages(
-                    uid,
-                    System.currentTimeMillis(),
-                    "Hello",
-                    auth.currentUser?.email!!)
-            )
+                .push()
+                .setValue(
+                    Messages(
+                        uid,
+                        System.currentTimeMillis(),
+                        input.text.toString(),
+                        auth.currentUser?.email!!
+                    )
+                )
+        }
+
+
+
+        val query = db.child("messages")
+
+        val options = FirebaseListOptions.Builder<Messages>()
+            .setLayout(R.layout.item_chat)
+            .setQuery(query, Messages::class.java)
+            .build()
+
+        val adapter = object : FirebaseListAdapter<Messages>(options) {
+            override fun populateView(v: View, model: Messages, position: Int) {
+                v.message.text = model.msg
+                v.name.text = model.email
+                v.time.text = model.time.toString()
+            }
+
+            override fun onDataChanged() {
+                super.onDataChanged()
+            }
+
+        }
+        adapter.startListening()
+
+        messages.adapter = adapter
+
     }
 }
 
